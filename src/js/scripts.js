56c5b0d11d00a91938c58ac25a458fac0486751a
Ashley Kolodziej <kolodz10@bu.edu>
2020-12-16 23:24:15 -0500

    Add color picker, skill select, content

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,20 @@
 // Add your scripts here
+
+import iro from '@jaames/iro';
+
+const colorPicker = new iro.ColorPicker("#picker", {
+  // Set the size of the color picker
+  width: 320,
+  // Set the initial color to pure red
+  colors: [
+    'rgb(100%, 0, 0)', // pure red
+    'rgb(0, 100%, 0)', // pure green
+    'rgb(0, 0, 100%)', // pure blue
+  ],
+  layout: [
+    {
+      component: iro.ui.Wheel,
+      options: {}
+    },
+  ]
+});
